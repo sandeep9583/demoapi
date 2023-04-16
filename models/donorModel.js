065a970632d2ee33e8dbd7ab3e5d34bb1db1e2d0
Sandeep Bandaru <46223856+sandeep9583@users.noreply.github.com>
2023-04-16 14:35:45 +0530

    donor

---
+++ models/donorModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const donorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  height: { type: Number, required: true },
+  weight: { type: Number, required: true },
+  location: {
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  contact: {
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+  },
+  bloodType: { type: String, required: true },
+  lastDonationDate: { type: Date, required: true },
+});
+
+const Donor = mongoose.model("Donor", donorSchema);
+
+module.exports = Donor;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/productModel");
+const Donor = require("./models/donorModel");
 const app = express();
 
 app.use(express.json());
@@ -75,6 +76,128 @@ app.delete("/products/:id", async (req, res) => {
         .json({ message: `cannot find any product with ID ${id}` });
     }
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// GET all donors
+app.get("/donors", async (req, res) => {
+  try {
+    const { bloodType, city, state, country } = req.query;
+
+    const query = {};
+    if (bloodType) {
+      query.bloodType = bloodType;
+    }
+    if (city) {
+      query["location.city"] = city;
+    }
+    if (state) {
+      query["location.state"] = state;
+    }
+    if (country) {
+      query["location.country"] = country;
+    }
+    const donors = await Donor.find(query);
+    res.json(donors);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// GET one donor
+app.get("/donors/:id", async (req, res) => {
+  try {
+    const donor = await Donor.findById(req.params.id);
+    if (donor) {
+      res.json(donor);
+    } else {
+      res.status(404).json({ message: "Donor not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// CREATE a donor
+app.post("/donors", async (req, res) => {
+  const donor = new Donor(req.body);
+
+  try {
+    const newDonor = await donor.save();
+    res.status(201).json(newDonor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// UPDATE a donor
+app.patch("/donors/:id", async (req, res) => {
+  try {
+    const donor = await Donor.findById(req.params.id);
+
+    if (donor) {
+      if (req.body.name != null) {
+        donor.name = req.body.name;
+      }
+      if (req.body.age != null) {
+        donor.age = req.body.age;
+      }
+      if (req.body.gender != null) {
+        donor.gender = req.body.gender;
+      }
+      if (req.body.height != null) {
+        donor.height = req.body.height;
+      }
+      if (req.body.weight != null) {
+        donor.weight = req.body.weight;
+      }
+      if (req.body.location != null) {
+        if (req.body.location.city != null) {
+          donor.location.city = req.body.location.city;
+        }
+        if (req.body.location.state != null) {
+          donor.location.state = req.body.location.state;
+        }
+        if (req.body.location.country != null) {
+          donor.location.country = req.body.location.country;
+        }
+      }
+      if (req.body.contact != null) {
+        if (req.body.contact.email != null) {
+          donor.contact.email = req.body.contact.email;
+        }
+        if (req.body.contact.phone != null) {
+          donor.contact.phone = req.body.contact.phone;
+        }
+      }
+      if (req.body.bloodType != null) {
+        donor.bloodType = req.body.bloodType;
+      }
+      if (req.body.lastDonationDate != null) {
+        donor.lastDonationDate = req.body.lastDonationDate;
+      }
+      const updatedDonor = await donor.save();
+      res.json(updatedDonor);
+    } else {
+      res.status(404).json({ message: "Donor not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// DELETE a donor
+app.delete("/donors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const donor = await Donor.findByIdAndDelete(id);
+    if (!donor) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any donor with ID ${id}` });
+    }
+    res.status(200).json(donor);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
